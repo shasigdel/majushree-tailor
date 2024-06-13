e6450604d7d437402e94d31761676b8799f17ab8
Shashank Sigdel <shashanksigdel@ShashanksLaptop.lan>
2024-06-13 18:32:06 -0400

    Change footer with maker name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,79 @@ const Footer = () => {
       <div className="contact-info">
         <h2>Contact Us</h2>
         <p>Manju Sigdel</p>
-        <p><a href="mailto:rajamkc@yahoo.com">rajamkc@yahoo.com</a></p>
-        <p><a href="tel:+13474076923">+1 347 407 6923</a></p>
+        <p>
+          <a href="mailto:rajamkc@yahoo.com">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="16"
+              height="16"
+              className="icon"
+            >
+              <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.89 2 1.99 2H20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"/>
+            </svg>
+            rajamkc@yahoo.com
+          </a>
+        </p>
+        <p>
+          <a href="tel:+13474076923">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="16"
+              height="16"
+              className="icon"
+            >
+              <path d="M6.62 10.79a15.053 15.053 0 0 0 6.59 6.59l2.2-2.2c.27-.27.68-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.24.2 2.45.57 3.57.12.34.03.74-.25 1.02l-2.2 2.2z"/>
+            </svg>
+            +1 347 407 6923
+          </a>
+        </p>
+      </div>
+      <div className="contact-info">
+        <h2>Website By:</h2>
+        <p>
+          <a href="https://www.linkedin.com/in/shashanksigdel1429/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="16"
+              height="16"
+              className="icon"
+            >
+              <path d="M19 3A2 2 0 0 1 21 5v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14zm-3.5 15v-6.5H13v6.5h2.5zm-1.25-7.25c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM11 18V11.5H8.5V18H11zM9.25 10.25c.69 0 1.25-.56 1.25-1.25S9.94 7.75 9.25 7.75 8 8.31 8 9s.56 1.25 1.25 1.25z"/>
+            </svg>
+            Shashank Sigdel
+          </a>
+        </p>
+        <p>
+          <a href="mailto:shasigdel@gmail.com">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="16"
+              height="16"
+              className="icon"
+            >
+              <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.89 2 1.99 2H20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"/>
+            </svg>
+            shasigdel@gmail.com
+          </a>
+        </p>
+        <p>
+          <a href="tel:+19296029703">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="16"
+              height="16"
+              className="icon"
+            >
+              <path d="M6.62 10.79a15.053 15.053 0 0 0 6.59 6.59l2.2-2.2c.27-.27.68-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.24.2 2.45.57 3.57.12.34.03.74-.25 1.02l-2.2 2.2z"/>
+            </svg>
+            +1 929 602 9703
+          </a>
+        </p>
       </div>
     </footer>
   );
